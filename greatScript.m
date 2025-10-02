b5814a8b05ded271eb59ee19970d03613970b445
Eric Severson <sever@umn.edu>
2025-10-02 06:53:42 -0500

    Add axes labels

--- greatScript.m
+++ greatScript.m
@@ -5,3 +5,7 @@ theta = linspace(0, 2*pi, 1000);
 y = sin(theta);
 figure
 plot(theta, y);
+
+%get some labels on those axes!
+xlabel('$\theta$ [radians]', 'Interpreter', 'latex')
+ylabel('who knows')
