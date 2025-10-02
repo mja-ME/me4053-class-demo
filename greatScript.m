8d7e81e931752504370a1dd4b7feac4bad6b7201
mja-ME <mja@umn.edu>
2025-10-02 10:51:13 -0500

    Update greatScript.m
    added a better plot as I don't like the legend

--- greatScript.m
+++ greatScript.m
@@ -15,3 +15,14 @@ xlim([0 2*pi])
 ylim([-1 1])
 
 legend('Sine function')
+
+%% Lets make a much better figure
+figure('Name',"This fig is better than Prof. Severson's")
+
+plot(theta, y,LineWidth=2);
+
+% format
+xlim([0 2*pi])
+ylim([-1.1 1.1])
+xlabel('$\theta$ [radians]', 'Interpreter', 'latex')
+ylabel('I know what this means', 'Interpreter', 'latex')
